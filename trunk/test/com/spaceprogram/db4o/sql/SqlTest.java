400adcc1092be96c6d8c5ae20c2faa958f4ba8c7
treeder <treeder@cfb21738-f819-0410-95d3-9d93f69a500d>
2006-08-03 01:39:18 +0000

    Changed from jdbc style to straight up sql

--- trunk/test/com/spaceprogram/db4o/sql/SqlTest.java
+++ trunk/test/com/spaceprogram/db4o/sql/SqlTest.java
@@ -5,16 +5,13 @@ import com.db4o.ObjectServer;
 import com.db4o.query.Query;
 import com.spaceprogram.db4o.Contact;
 import com.spaceprogram.db4o.TestUtils;
+import com.spaceprogram.db4o.sql.parser.SQLParseException;
 import com.spaceprogram.db4o.util.DbUtil;
-import com.spaceprogram.db4o.sql.jdbc.Db4oConnection;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.List;
 
 /**
@@ -24,25 +21,12 @@ import java.util.List;
  */
 public class SqlTest {
 
-    public static void main(String[] args) {
-        // intellij not supporting junit4 yet
-        SqlTest test = new SqlTest();
-        test.setupDb();
-        try {
-            test.testQueryResults();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        test.tearDown();
-    }
-
     static ObjectServer server;
 
     @BeforeClass
     public static void setupDb() {
         System.out.println("Setup");
         server = DbUtil.getObjectServer("sqltest");
-
         ObjectContainer oc = server.openClient();
         TestUtils.makeContacts(oc, 10);
         //TestUtils.dump(oc);
@@ -61,13 +45,6 @@ public class SqlTest {
         server.close();
     }
 
-
-    String queriesToTest[] = new String[]{
-            "select *" // should fail
-            , "select * from Contact " //should work fine
-            , "from Contact" // should work same as above
-    };
-
     /**
      * - test query time vs normal soda query
      * - test that correct number of results are returned
@@ -85,7 +62,7 @@ public class SqlTest {
             ObjectContainer oc = server.openClient();
             Query q = oc.query();
             q.constrain(Contact.class);
-            //q.descend("name").constrain("contact 2");
+            q.descend("name").constrain("contact 2");
             q.descend("category").constrain("friends");
             long startTime = System.currentTimeMillis();
             List results = q.execute();
@@ -100,43 +77,40 @@ public class SqlTest {
         }
 
         {
-            // now same query with jdbc
+            // now same query with sql
             System.out.println("SQL query");
             // get JDBC connection
-            Connection conn = getConnection();
+
+             ObjectContainer oc = server.openClient();
             try {
-                // create JDBC statement
-                Statement stmt = conn.createStatement();
                 long startTime = System.currentTimeMillis();
 
                 // execute query
-                ResultSet rs = stmt.executeQuery("select * from db4o.Contact c where " +
-                        // "name = 'contact 2' and " + //  and email = 'email@2.com'
-                        " category = 'friends'"
-                );
+                String query = "select * from com.spaceprogram.db4o.Contact c where " +
+                        "name = 'contact 2' and " + //  and email = 'email@2.com'
+                        " category = 'friends'";
+
+                SqlQuery sqlQuery = (SqlQuery) SqlParser.parse(query);
+                List results = SQLToSoda.execute(oc, sqlQuery);
+
                 int counter = 0;
-                while (rs.next()) {
-                    String name = rs.getString("name");
-                    System.out.println("Got: " + name);
+                for (Object result : results) {
+                    System.out.println("Got: " + result);
                     counter++;
                 }
                 long endTime = System.currentTimeMillis();
                 long duration = endTime - startTime;
                 System.out.println("SQL duration: " + duration);
-                rs.close();
-                stmt.close();
-                //Assert.assertEquals(1, counter);
 
-            } catch (SQLException e) {
+
+            } catch (SQLParseException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
                 e.printStackTrace();
             } finally {
-                if (conn != null) conn.close();
+                oc.close();
             }
         }
-    }
-
-    private Connection getConnection() {
-        return new Db4oConnection(server.openClient());
     }
 
 
